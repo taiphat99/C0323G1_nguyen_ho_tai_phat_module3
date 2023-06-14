e5e21ab8440327890a167c646c9cb4272ee5e55b
taiphat99 <taiphatlqp@gmail.com>
2023-06-14 20:26:06 +0700

    a

--- teacherDemo.sql
+++ teacherDemo.sql
@@ -1,12 +1,38 @@
-SELECT * FROM teacher_management.teacher;
+-- display
+select * from teacher_management.teacher;
 
-insert into teacher(name,age,email,gender)
-values("Jenny",18,"jenny@gmail.com",0);
+select id,name,age,email from teacher_management.teacher
+where gender = 1;
 
-update teacher 
+-- check safe update mode
+SHOW VARIABLES LIKE 'SQL_SAFE_UPDATES';
+set sql_safe_updates = 0;
+
+-- add
+insert into teacher_management.teacher(name,age,email,gender)
+values("Hulk",32,"hulk@gmail.com",1);
+
+-- update
+update teacher_management.teacher 
 set name = "Kellie", gender = false, email = "kellie@gmail.com"
 where id = 3;
 
-select name,id,age,email from teacher 
-where gender = 1;
+
+update teacher_management.teacher 
+set salary = 20000000
+where gender = 0;
+
+update teacher_management.teacher 
+set salary = 35000000
+where age >= 30;
+
+alter table teacher_management.teacher 
+add column salary decimal(12,2);
+
+-- delete
+alter table teacher_management.teacher 
+drop column email;
+
+
+
 
