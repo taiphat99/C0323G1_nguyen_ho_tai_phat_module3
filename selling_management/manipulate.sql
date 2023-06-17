c2bf0c39aff5ef615ad005312860f1e2fdb0ff67
taiphat99 <taiphatlqp@gmail.com>
2023-06-17 23:29:28 +0700

    a

--- selling_management/manipulate.sql
+++ selling_management/manipulate.sql
@@ -1,3 +1,5 @@
+-- insert
+
 insert into customer_info
 values 
 (1,"Minh Quan",10),
@@ -27,3 +29,50 @@ values
 (3,1,8),
 (2,5,4),
 (2,3,3);
+
+
+/*SELECT
+Hiển thị các thông tin gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng order*/
+
+SELECT 
+    order_id, order_date, order_total_price
+FROM
+    order_info;
+
+--/*Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách*/
+
+SELECT 
+    c.customer_id, c.customer_name, c.customer_age
+FROM
+    order_info o
+        JOIN
+    customer_info c ON o.customer_id = c.customer_id
+GROUP BY c.customer_id;
+
+/*Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào*/
+
+SELECT 
+    *
+FROM
+    customer_info c
+        LEFT JOIN
+    order_info o ON c.customer_id = o.customer_id
+WHERE
+    o.order_id <=> NULL;
+
+/*Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn
+(Giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)*/
+
+SELECT 
+    oi.order_id,
+    oi.order_date,
+    p.product_name,
+    p.product_price * od.order_detail_quantity AS total
+FROM
+    order_detail od
+        JOIN
+    product_info p ON od.product_id = p.product_id
+        JOIN
+    order_info oi ON od.order_id = oi.order_id
+
+
--- student_management/manipulate.sql
+++ student_management/manipulate.sql
@@ -1,31 +1,48 @@
 -- Hiển thị tất cả các sinh viên có tên bắt đầu bằng ký tự ‘h’
 
-select  *
-from student 
-where student_name like "h%";
+SELECT 
+    *
+FROM
+    student
+WHERE
+    student_name LIKE 'h%';
 
 -- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
 
-select *
-from class
-where month(start_date)=12;
+SELECT 
+    *
+FROM
+    class
+WHERE
+    MONTH(start_date) = 12;
 
 -- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
 
-select *
-from subject
-where 3<=credit and credit <= 5;
+SELECT 
+    *
+FROM
+    subject
+WHERE
+    3 <= credit AND credit <= 5;
 
 -- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
-update student
-set class_id = 2
-where student_name ="Hung";
+
+UPDATE student 
+SET 
+    class_id = 2
+WHERE
+    student_name = 'Hung';
+
 
 -- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
 
-select student.student_name,subject.sub_name,mark.mark
-from mark
-join student on mark.student_id = student.student_id
-join subject on mark.sub_id = subject.sub_id
-order by mark.mark;
+SELECT 
+    student.student_name, subject.sub_name, mark.mark
+FROM
+    mark
+        JOIN
+    student ON mark.student_id = student.student_id
+        JOIN
+    subject ON mark.sub_id = subject.sub_id
+ORDER BY mark.mark;
 
